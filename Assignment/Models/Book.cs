dba7c434010d7dca7925261607854db3409808f7
mohammadsaadafridi <55627981+mohammadsaadafridi@users.noreply.github.com>
2020-09-14 10:47:20 +0500

    first commit

---
+++ Assignment/Models/Book.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+
+namespace Assignment.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        [Required]
+        public string Title { get; set; }
+
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+
+
+    public class BookDBContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+    }
+}
